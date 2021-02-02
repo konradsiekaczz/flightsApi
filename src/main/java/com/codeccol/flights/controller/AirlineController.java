2d559c7bbee38b5858ac41b17a9db521dd6b93be
konradsiekaczz <siekaczkonrad@gmail.com>
2021-02-02 17:16:25 +0100

    Add method getAirline in controller

--- src/main/java/com/codeccol/flights/controller/AirlineController.java
+++ src/main/java/com/codeccol/flights/controller/AirlineController.java
@@ -1,4 +1,18 @@
 package com.codeccol.flights.controller;
 
+import com.codeccol.flights.model.Airline;
+import com.codeccol.flights.service.AirlineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class AirlineController {
+    private final AirlineService airlineService;
+
+    @GetMapping("/airline")
+    public Airline getAirline() {
+        return airlineService.getAirline();
+    }
 }
