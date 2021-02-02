4f7975b011066b58621f0f6a2da63cebac4ee377
konradsiekaczz <siekaczkonrad@gmail.com>
2021-02-02 17:36:57 +0100

    Create getInfoAirlines method

--- src/main/java/com/codeccol/flights/service/AirlineService.java
+++ src/main/java/com/codeccol/flights/service/AirlineService.java
@@ -9,12 +9,17 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class AirlineService {
 
+    public static final String FLY_API_URL = "http://aviation-edge.com/v2/public/flights?key=ae64ea-f17efc";
     private RestTemplate restTemplate = new RestTemplate();
 
     public Airline getAirline(){
-       String response =  restTemplate.getForObject("http://aviation-edge.com/v2/public/flights?key=ae64ea-f17efc&airlineIata=W8",
-                String.class);
+       String response = getInfoForAirlines("W8");
        log.info(response);
         return null;
+    }
+
+    private String getInfoForAirlines(String iataNumber) {
+        return restTemplate.getForObject(FLY_API_URL + "&airlineIata={iataNumber}",
+                String.class, iataNumber);
     }
 }
